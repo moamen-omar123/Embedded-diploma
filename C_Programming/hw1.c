53a798d00e8f3aebf64171b67b36aa17d114d41e
moamen <moamen.omar@gmail.com>
2025-08-22 17:57:25 +0300

    Frst Question

---
+++ C_Programming/hw1.c
@@ -0,0 +1,15 @@
+/*
+ * hw1.c
+ *
+ *  Created on: Aug 22, 2025
+ *      Author: LOQ
+ */
+#include <stdio.h>
+
+int main() {
+    printf("##########Console-output###\n");
+    printf("C Programming\n");
+    return 0;
+}
+
+
