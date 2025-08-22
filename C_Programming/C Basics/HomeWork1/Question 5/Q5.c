77e104bc5501f1f4db86d0b99e513c0db187b676
moamen <moamen.omar@gmail.com>
2025-08-22 19:29:24 +0300

    Question 5 added

---
+++ C_Programming/C Basics/HomeWork1/Question 5/Q5.c
@@ -0,0 +1,15 @@
+/*
+ * Q5.c
+ *
+ *  Created on: Aug 22, 2025
+ *      Author: LOQ
+ */
+# include <stdio.h>
+int main(){
+	char x ;
+	printf("Enter a character: \n");
+	 fflush(stdout);
+	scanf(" %c",&x);
+	printf("The ASCII value of '%c' is %d\n", x, x);
+	return 0;
+}
