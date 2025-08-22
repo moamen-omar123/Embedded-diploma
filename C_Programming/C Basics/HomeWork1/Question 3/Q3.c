257e2f6df77031de15f31a2e1318806b927fee96
moamen <moamen.omar@gmail.com>
2025-08-22 18:53:02 +0300

    Question 3 added

---
+++ C_Programming/C Basics/HomeWork1/Question 3/Q3.c
@@ -0,0 +1,19 @@
+/*
+ * Q3.c
+ *
+ *  Created on: Aug 22, 2025
+ *      Author: LOQ
+ */
+#include <stdio.h>
+int main () {
+	int x,y,sum;
+	fflush(stdin); fflush(stdout);
+	printf("Enter two integers: \n");
+	fflush(stdin); fflush(stdout);
+	scanf("%d %d",&x,&y);
+	fflush(stdin); fflush(stdout);
+	sum = x +y;
+	printf("sum = %d\n",sum);
+	return 0;
+}
+
